d6019ce7b49c2b8ee58d81e8972f6195dbb393f2
Илья Петров <ipetrov@gameslab.store>
2025-09-13 13:08:22 +0300

    Update job1.sh

--- test/job1.sh
+++ test/job1.sh
@@ -1,16 +1,13 @@
-!/bin/bash
+#!/bin/bash
 
-SVATCH --job-name="09.25"
-
-SBATCH --partition=debug
-
-SBATCH --nodes=10
-
-SBATCH --time=7-00:01:00
-SBATCH --ntasks-per-node=1
-
-SBATCH --mem=1992
+#SBATCH --job-name="09.25"
+#SBATCH --partition=debug
+#SBATCH --nodes=10
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=7-00:01:00
+#SBATCH --mem=1992
 
 echo "HOSTNAME = $HOSTNAME"
+echo "SLURM_JOB_NODELIST = $SLURM_JOB_NODELIST"
 
-mpirun -np 10 m
+mpirun -np 10 ./your_mpi_program
