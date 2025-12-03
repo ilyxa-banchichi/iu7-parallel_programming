501a0f588adcbb784e9b44043a45bae20793d3b9
Илья Петров <ipetrov@gameslab.store>
2025-12-04 02:22:58 +0300

    Update job.sh

--- task/mpi/job.sh
+++ task/mpi/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name="Ilya Task program"
 #SBATCH --partition=debug
-#SBATCH --nodes=10
+#SBATCH --nodes=11
 #SBATCH --time=0-00:02:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=1992
