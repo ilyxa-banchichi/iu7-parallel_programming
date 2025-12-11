6ba96979fe825e37f50037b8d61e98d6b70f066c
Илья Петров <ipetrov@gameslab.store>
2025-12-11 03:38:45 +0300

    Added jobs

---
+++ task/omp/jobs/job_1_parall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name="Ilya Task program"
+#SBATCH --partition=debug
+#SBATCH --nodes=1
+#SBATCH --time=0-00:02:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=1992
+
+mpirun -np 1 build/main_parall
--- task/omp/scripts/build.sh
+++ task/omp/scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mpicc -DUSE_MULTITHREAD -fopenmp mpi_main.c -o build/main_mpi_parall
-mpicc -DUSE_MULTITHREAD -fopenmp main.c -o build/main_parall
 mpicc -fopenmp mpi_main.c -o build/main_mpi_sync
+
+mpicc -DUSE_MULTITHREAD -fopenmp main.c -o build/main_parall
 mpicc -fopenmp main.c -o build/main_sync
