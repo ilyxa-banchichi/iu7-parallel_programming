f28f0f038dc54cf63af348ea1b3273ec48acb581
Илья Петров <ipetrov@gameslab.store>
2025-12-11 03:48:39 +0300

    Replace files

--- task/mpi/job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name="Ilya Task program"
-#SBATCH --partition=debug
-#SBATCH --nodes=10
-#SBATCH --time=0-00:02:00
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem=1992
-
-mpirun -np 10 main
